0dc3b852e6dae41ccad1ab81278aea8af19f32ef
abdou059 <abdoumonuir9@gmail.com>
2022-09-21 16:29:49 +0300

    create fetch api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import Header from './component/Header';
+import Header from './Components/Header';
 import './App.css'
 import Carts from './Components/Carts';
 
 class App extends Component {
-
-    render() { 
+    
+    
+    state = {
+        result: null,
+        isLoading:true
+    }
+    componentDidMount() {
+        const getData = () => {
+            fetch('https://api.themoviedb.org/3/movie/popular?api_key=7b275ef8c426a1457421f07013981104')
+                .then(data => data.json())
+                .then(result => this.setState({
+                    result,
+                    isLoading:false
+                }))
+                .catch()
+        }
+        getData()
+    }
+    render() {
+        console.log(this.state.result)
+        if(!this.state.result) return <h1>Loading...</h1>
         return (
             <div>
-                <Header />
-                <Carts/>
+                <Header firstMove={this.state.result.results[0]} />
+                <Carts />
             </div>
         );
     }
